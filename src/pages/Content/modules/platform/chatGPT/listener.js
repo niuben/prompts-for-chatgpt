940b48c5ace58d40c0ae94edf9027e4bfee9a7f7
goto_niuben <goto_niuben@git.cloud.tencent.com>
2023-07-29 08:28:34 +0800

    增加监听逻辑

---
+++ src/pages/Content/modules/platform/chatGPT/listener.js
@@ -0,0 +1,23 @@
+/* 监听chatGPT 输方法;  */ 
+
+import $ from "jquery";
+
+export function onSend(callback){
+    
+    // 判断是否存在
+    if($("#prompt-textarea").length == 0) return;
+    
+    // 监听输入框回车事件, 回车时触发回调函数;
+    $("#prompt-textarea").off("keydown");
+    $("#prompt-textarea").on("keydown", (e)=>{
+        if(e.keyCode == 13) callback && callback(e);
+    });
+
+    // 监听输入框右侧按钮，点击后触发回调函数;
+    $("#prompt-textarea").next().off("click");
+    $("#prompt-textarea").next().on("click", (e)=>{
+        callback && callback(e);
+    });    
+
+}
+
